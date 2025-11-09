04fce6159ca678a34c110ca4122fb6a62cf86d56
mahmudurmahid <m.mahid@icloud.com>
2025-11-09 10:46:56 +0000

    q2 solution added

--- Q02JavaStdinStdout.java
+++ Q02JavaStdinStdout.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
 public class Q02JavaStdinStdout {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
