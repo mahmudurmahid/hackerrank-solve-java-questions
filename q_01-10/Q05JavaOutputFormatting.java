1f0578daf6d78043ae345b31924e3139abd5c66e
mahmudurmahid <m.mahid@icloud.com>
2025-11-10 19:17:37 +0000

    q5 admin

---
+++ q_01-10/Q05JavaOutputFormatting.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Q05JavaOutputFormatting {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                
+    }
+    System.out.println("================================");
+}
