209a1cb903ba0a7e37fc3ca810f9cbf60aeb9d0a
mahmudurmahid <m.mahid@icloud.com>
2025-11-10 19:14:59 +0000

    q4 solution added

--- q_01-10/Q04JavaStdinStdoutII.java
+++ q_01-10/Q04JavaStdinStdoutII.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 public class Q04JavaStdinStdoutII {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
         int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
 
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
