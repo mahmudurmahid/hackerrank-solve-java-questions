dc20de0292f92047896c95042d6ac72ae1662f4a
mahmudurmahid <m.mahid@icloud.com>
2025-11-10 15:09:55 +0000

    q3 solution reduced amount of code

--- q03JavaIfElse.java
+++ q03JavaIfElse.java
@@ -13,13 +13,9 @@ public class q03JavaIfElse {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        if (N % 2 != 0) {
+        if (N % 2 != 0 || (N % 2 == 0 && N >= 6 && N <= 20)) {
             System.out.println("Weird");
-        } else if (N % 2 == 0 && N >= 2 && N <= 5) {
-            System.out.println("Not Weird");
-        } else if (N % 2 == 0 && N >= 6 && N <= 20) {
-            System.out.println("Weird");
-        } else if (N % 2 == 0 && N > 20) {
+        } else if ((N % 2 == 0 && N >= 2 && N <= 5) || (N % 2 == 0 && N > 20)) {
             System.out.println("Not Weird");
         }
 
