715ad0a4f518325392ffb5b42afc2e43c39a3087
mahmudurmahid <m.mahid@icloud.com>
2025-11-13 13:40:09 +0000

    q4 static values added to avoid typing in a, b, c values

--- q_01-10/Q04JavaStdinStdoutII.java
+++ q_01-10/Q04JavaStdinStdoutII.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
 
 public class Q04JavaStdinStdoutII {
+    static String s = "Welcome to HackerRank's Java tutorials!";
+    static double d = 3.1415;
+    static int i = 42;
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        // Scanner scan = new Scanner(System.in);
         
-        int i = scan.nextInt();
-        double d = scan.nextDouble();
-        scan.nextLine();
-        String s = scan.nextLine();
+        // int i = scan.nextInt();
+        // double d = scan.nextDouble();
+        // scan.nextLine();
+        // String s = scan.nextLine();
 
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
