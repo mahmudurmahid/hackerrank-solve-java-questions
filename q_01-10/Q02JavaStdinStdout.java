e92ddd5b83ba3d25a11cf84b7a0f9d12bbb5ee99
mahmudurmahid <m.mahid@icloud.com>
2025-11-13 13:32:32 +0000

    q2 static ints added to avoid typing in a, b, c values

--- q_01-10/Q02JavaStdinStdout.java
+++ q_01-10/Q02JavaStdinStdout.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
 
 public class Q02JavaStdinStdout {
+    static int a = 42;
+    static int b = 100;
+    static int c = 125;
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int c = scan.nextInt();
+        // Scanner scan = new Scanner(System.in);
+        // int a = scan.nextInt();
+        // int b = scan.nextInt();
+        // int c = scan.nextInt();
 
         System.out.println(a);
         System.out.println(b);
--- q_01-10/Q05JavaOutputFormatting.java
+++ q_01-10/Q05JavaOutputFormatting.java
@@ -10,4 +10,5 @@ public class Q05JavaOutputFormatting {
                 
     }
     System.out.println("================================");
+    }
 }
